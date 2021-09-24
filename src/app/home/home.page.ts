00648409b8be2f94ef6b71fabd365afa4dc0a945
KaioMorais34 <kaio@zeester.com.br>
2021-09-24 12:51:09 -0300

    :zap: Adiciona a informação de 'Classificação' do IMC sobre o cálculo do resultado final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,44 +1,67 @@
-import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
   weight: number;
   height: number;
 
   constructor(private toastController: ToastController) {}
 
   isFormValid() {
-    return (this.height && this.weight && this.height > 0 && this.weight > 0);
+    return this.height && this.weight && this.height > 0 && this.weight > 0;
   }
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+
+    let classification = "Não definida";
+
+    switch (true) {
+      case imc < 18.5:
+        classification = "Magreza";
+        break;
+
+      case imc >= 18.5 && imc <= 24.9:
+        classification = "Normal";
+        break;
+
+      case imc >= 25.0 && imc <= 29.9:
+        classification = "Sobrepeso";
+        break;
+
+      case imc >= 30.0 && imc <= 39.9:
+        classification = "Obesidade";
+        break;
+
+      case imc >= 40.0:
+        classification = "Obesidade Grave";
+
+      default:
+        break;
+    }
+
+    this.showMessage(`IMC = ${imc.toFixed(2)}\nClassificação = ${classification}`);
   }
 
   async showMessage(msg: string) {
     const previousToast = await this.toastController.getTop();
-    if (previousToast) {
-      await this.toastController.dismiss();
-    }
 
-    const toast = await this.toastController.create(
-      {
-        message: msg,
-        color: 'light',
-        buttons: [
-          {
-            icon: 'close'
-          }
-        ]
-      }
-    );
+    if (previousToast) await this.toastController.dismiss();
+
+    const toast = await this.toastController.create({
+      message: msg,
+      color: "light",
+      buttons: [
+        {
+          icon: "close",
+        },
+      ],
+    });
     toast.present();
   }
 }
